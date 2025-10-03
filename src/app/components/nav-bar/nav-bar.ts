bf210706da82d8f8d6c8119973475c92e1d0c7ba
Darius Simonca <simonca.darius48@yahoo.com>
2025-10-03 11:44:47 +0300

    website done <3

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,17 +1,29 @@
 import { NgClass } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [NgClass],
+  imports: [NgClass, FaIconComponent],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss'
 })
 export class NavBar {
+  
   isScrolled = false;
-
+  faBars = faBars;
+  isClicked = false;
+  
+  
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.isScrolled = window.scrollY > 99;
   }
+
+  showMobileMenu(){
+    this.isClicked = !this.isClicked;
+  }
+
 }
